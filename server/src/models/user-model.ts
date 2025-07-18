c79295b958276c4374fbdca86b9a38c71ea8f5f8
sharon504 <sharonpshajan@gmail.com>
2025-07-18 18:28:17 +0530

    Adding user model

---
+++ server/src/models/user-model.ts
@@ -0,0 +1,86 @@
+import { v4 as uuid } from 'uuid';
+import { DataTypes, Model, Optional } from 'sequelize';
+import { sequelize } from '@/utils';
+import bcrypt from 'bcryptjs';
+
+interface UserAttributes {
+  id?: string;
+  email: string;
+  password: string;
+  role: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+interface UserCreationAttributes extends Optional<UserAttributes, 'id'> {
+  verifyPassword(
+    this: UserCreationAttributes,
+    password: string
+  ): Promise<boolean>;
+}
+
+class User
+  extends Model<UserAttributes, UserCreationAttributes>
+  implements UserAttributes
+{
+  public id!: string;
+  public email!: string;
+  public password!: string;
+  public role!: string;
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+
+  public async verifyPassword(password: string): Promise<boolean> {
+    return await bcrypt.compare(password, this.password);
+  }
+
+  public static async findByEmail(email: string): Promise<User | null> {
+    return await this.findOne({ where: { email } });
+  }
+
+  public static async createUser(
+    userData: UserCreationAttributes
+  ): Promise<User> {
+    return await this.create(userData);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: () => uuid(),
+      primaryKey: true,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      set(value: string) {
+        const hashedPassword = bcrypt.hashSync(value, 10);
+        this.setDataValue('password', hashedPassword);
+      },
+    },
+    role: {
+      type: DataTypes.UUID,
+      allowNull: true,
+      unique: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'User',
+    tableName: 'users',
+    timestamps: true,
+  }
+);
+
+export default User;
