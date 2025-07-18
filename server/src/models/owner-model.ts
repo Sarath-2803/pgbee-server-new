92edd929319b7afef68dd3a36a0dc0c38e7f8445
sharon504 <sharonpshajan@gmail.com>
2025-07-18 18:27:18 +0530

    Adding owner model

---
+++ server/src/models/owner-model.ts
@@ -0,0 +1,74 @@
+import { v4 as uuid } from 'uuid';
+import { DataTypes, Model, Optional } from 'sequelize';
+import { sequelize } from '@/utils';
+
+interface OwnerAttributes {
+  id?: string;
+  name?: string;
+  phone?: string;
+  address?: string;
+  location?: string;
+  files?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+// interface OwnerCreationAttributes
+//   extends Optional<OwnerAttributes, 'id' | 'files'> {}
+
+class Owner extends Model<OwnerAttributes> implements OwnerAttributes {
+  public id!: string;
+  public name?: string;
+  public phone?: string;
+  public address?: string;
+  public location?: string;
+  public files?: string;
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+  public static async findById(id: string): Promise<Owner | null> {
+    return await this.findOne({ where: { id } });
+  }
+  public static async createOwner(
+    ownerData: Optional<OwnerAttributes, 'id' | 'files'>
+  ): Promise<Owner> {
+    return await this.create(ownerData);
+  }
+}
+
+Owner.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: () => uuid(),
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    files: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Owner',
+    tableName: 'owners',
+    timestamps: true,
+  }
+);
+
+export default Owner;
---
+++ server/src/models/relations.ts
@@ -0,0 +1,25 @@
+import { Student, Owner, Role, User } from '@/models';
+
+Role.hasOne(User, {
+  foreignKey: {
+    name: 'roleId',
+    allowNull: false,
+  },
+  foreignKeyConstraint: true,
+});
+
+User.hasOne(Owner, {
+  foreignKey: {
+    name: 'userId',
+    allowNull: false,
+  },
+  foreignKeyConstraint: true,
+});
+
+User.hasOne(Student, {
+  foreignKey: {
+    name: 'userId',
+    allowNull: false,
+  },
+  foreignKeyConstraint: true,
+});
