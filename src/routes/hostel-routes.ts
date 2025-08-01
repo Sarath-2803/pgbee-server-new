25e8bef50dbd0e6bc8df2cc81e8ad81de4b3f67b
sharon504 <sharonpshajan@gmail.com>
2025-08-01 23:20:15 +0530

    Removing authorize middleware from getAllHostels

--- src/routes/hostel-routes.ts
+++ src/routes/hostel-routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { hostelController } from "@/controllers";
+import { authorize } from "@/middlewares";
 
 const hostelRouter = Router();
 
-hostelRouter.post("/", hostelController.regHostel);
+hostelRouter.post("/", authorize, hostelController.regHostel);
 hostelRouter.get("/", hostelController.getAllHostelsStudent);
-hostelRouter.get("/user", hostelController.getAllHostelsOwner);
-hostelRouter.put("/:id", hostelController.updateHostel);
-hostelRouter.delete("/:id", hostelController.deleteHostel);
+hostelRouter.get("/user", authorize, hostelController.getAllHostelsOwner);
+hostelRouter.put("/:id", authorize, hostelController.updateHostel);
+hostelRouter.delete("/:id", authorize, hostelController.deleteHostel);
 
 export default hostelRouter;
